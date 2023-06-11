ff2a737c88124c020c8038b4c0790e8194320138
LeniSmt <godofredu.fredugodo@gmail.com>
2023-06-11 17:18:41 -0300

    Create q8.c

---
+++ q8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num=2;
+    
+    printf("Todos os números pares de 1 até 100:\n");
+    while (num<=100)
+    {
+        printf("%d ", num);
+        num+=2;
+    }
+    printf("\nfim");
+    return 0;
+}
+
+
+
