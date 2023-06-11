ef5ebd74cc4eb718b2508db3eb546b7929f7ad30
LeniSmt <godofredu.fredugodo@gmail.com>
2023-06-11 17:19:24 -0300

    Create q30.c

---
+++ q30.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    
+    printf("Digite o tamanho do quadrado (entre 1 e 20): ");
+    scanf("%d", &t);
+    if (t>=1 && t<=20) 
+    {
+        
+        for (int i=0; i<t; i++) 
+        {
+            for (int j=0; j<t; j++) 
+            {
+                printf(" *");
+            }
+            printf("\n");
+        }
+    } 
+    else 
+    {
+        printf("Tamanho do lado inválido.\n");
+    }
+
+    return 0;
+}
